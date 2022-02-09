48f185a81ed885d4d11b4f10047e0d14ce14e84d
joanbelk <91218035+jbk95@users.noreply.github.com>
2022-02-09 15:35:31 -0400

    Create hello_world.cs

---
+++ hello_world.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Display
+{
+    public static void print()
+    {
+      //write your code here
+      Console.WriteLine("The movie quote is: My name is Bond. James Bond!");
+    }
+}
