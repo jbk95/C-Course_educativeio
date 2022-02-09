eec98e943fcd61fc08513abaec8b2bbaa566f38a
joanbelk <91218035+jbk95@users.noreply.github.com>
2022-02-09 15:42:28 -0400

    Create variables.cs

---
+++ variables.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program {
+  static void Main() {
+    int a=2;
+    char A='b';
+    float rt345=2.3F;
+    double squareArea=6.5; //camelCase
+    Console.WriteLine("Value of a is {0}",a);
+    Console.WriteLine("Value of A is {0}",A);
+    Console.WriteLine("Value of rt345 is {0}", rt345);
+    Console.WriteLine("Value of squareArea is {0}", squareArea);
+  }
+}
